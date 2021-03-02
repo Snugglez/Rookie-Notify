e11b77d4b098a09da54a6ac27b7a2ab71d6dfb04
Snugglez <32309975+Snugglez@users.noreply.github.com>
2021-03-02 17:27:55 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Rookie-Notify
-Simple tera toolbox mod that marks rookies in dungeons with an effect
+
+Simple tera toolbox mod that tells you if someone is a rookie by marking them with an effect
+
+## commands
+
+`/8 rn` to toggle on/off(default: on)
+`/8 rn somenumber` to change the effect that is used to mark rookies(default: 920043015)
+
+## recommended effects preview
+
+920043015 preview:
+[](https://i.gyazo.com/6ef693078e03d9baa7f0c33c66f97d71.png)
+
+514053041 preview:
+[](https://i.gyazo.com/6933e7b419a502e7bbb4bd530ffb76de.png)
+
+514053022 preview:
+[](https://i.gyazo.com/2b628baea7e5ac90a36b1f6121101216.png)
+
+915051106 preview:
+[](https://i.gyazo.com/2c6c05e7c6740e13a06448993c689948.png)
---
+++ index.js
@@ -0,0 +1,44 @@
+module.exports = function reee(d) {
+  let partyList, tempList = {}, tempAbnormal = {}
+  d.game.initialize("party");
+  d.game.party.on('list', (members) => { partyList = members; })
+
+  d.command.add('rn', (arg) => {
+    if (!arg) {
+      d.settings.enabled = !d.settings.enabled
+      d.command.message(`${d.settings.enabled ? 'en' : 'dis'}abled`)
+    }
+    if (arg) {
+      d.settings.effectId = parseInt(arg)
+      d.command.message(`effectId set to: ${d.settings.effectId}`)
+    }
+  })
+
+  d.hook('S_LOAD_TOPO', 'event', () => { if (!d.game.party.inParty()) { tempList = {} } })
+
+  d.hook('S_ABNORMALITY_BEGIN', '*', (e) => {
+    if ([4650, 4651, 4950, 4951, 4952, 4954].includes(e.id) && !tempList[e.target]) {
+      tempList[e.target] = true
+      d.command.message(`${d.game.party.getMemberData(e.target).name} is a rookie`)
+    }
+  })
+
+  d.hook('S_PARTY_MEMBER_ABNORMAL_ADD', '*', (e) => {
+    if ([4650, 4651, 4950, 4951, 4952, 4954].includes(e.id)) {
+      partyList.forEach(members => {
+        if (members.playerId == e.playerId && !tempList[members.gameId]) {
+          tempList[members.gameId] = true
+          d.command.message(`${members.name} is a rookie`)
+        }
+      })
+    }
+  })
+
+  //if effect isnt applying, I might need to add it back to abnormal hooks as well
+  d.hook('S_SPAWN_USER', '*', (e) => { if (tempList[e.gameId]) process.nextTick(() => { addAbnormal(e.gameId) }) })
+
+  d.hook('S_ABNORMALITY_END', '*', (e) => { if (tempList[e.target] && e.id == 90520) return false })
+
+  function addAbnormal(target) { d.send('S_PLAY_EFFECT', 1, { gameId: target, id: d.settings.effectId }) }
+
+}
