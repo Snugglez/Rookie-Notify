697dddfa17c820bb83a8d6beff78561cba420530
Snugglez <32309975+Snugglez@users.noreply.github.com>
2021-03-02 19:09:14 -0500

    had to kill the meme part :(

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ module.exports = function reee(d) {
   d.hook('S_ABNORMALITY_BEGIN', '*', (e) => {
     if ([4650, 4651, 4950, 4951, 4952, 4954].includes(e.id) && !tempList[e.target]) {
       tempList[e.target] = true
-      d.command.message(`${d.game.party.getMemberData(e.target).name} is a rookie`)
       addAbnormal(e.target)
     }
   })
@@ -29,7 +28,6 @@ module.exports = function reee(d) {
       partyList.forEach(members => {
         if (members.playerId == e.playerId && !tempList[members.gameId]) {
           tempList[members.gameId] = true
-          d.command.message(`${members.name} is a rookie`)
           addAbnormal(members.gameId)
         }
       })
@@ -40,6 +38,10 @@ module.exports = function reee(d) {
 
   d.hook('S_ABNORMALITY_END', '*', (e) => { if (tempList[e.target] && e.id == 90520) return false })
 
-  function addAbnormal(target) { d.send('S_PLAY_EFFECT', 1, { gameId: target, id: d.settings.effectId }) }
-
+  function addAbnormal(target) {
+    if (!d.game.me.is(target)) {//playEffect not being able to be cleared easily killed this as a meme :(
+      d.command.message(`${d.game.party.getMemberData(target).name} is a rookie`)
+      d.send('S_PLAY_EFFECT', 1, { gameId: target, id: d.settings.effectId })
+    }
+  }
 }
